016a56aceedf40df00a54836b6b39485e4e51fff
wannazid <wannaz.idn@gmail.com>
2022-10-15 07:48:46 +0700

    WP Installer, auto installer wp

---
+++ menu/main.py
@@ -0,0 +1,17 @@
+def home():
+	return ''' 
+
+
+  _       ______     ____           __        ____         
+ | |     / / __ \   /  _/___  _____/ /_____ _/ / /__  _____
+ | | /| / / /_/ /   / // __ \/ ___/ __/ __ `/ / / _ \/ ___/
+ | |/ |/ / ____/  _/ // / / (__  ) /_/ /_/ / / /  __/ /    
+ |__/|__/_/      /___/_/ /_/____/\__/\__,_/_/_/\___/_/     
+                                                          
+  by : wannazid
+  github : github.com/wannazid
+
+ [1] Auto Wordpress Install
+ [2] Auto Setup Config Wordpress (Coming Soon)
+
+	'''
